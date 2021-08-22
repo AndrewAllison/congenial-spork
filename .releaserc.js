d3ebe4eac5a1278b56db5417a99a61151c70ffe8
AndrewAllison <andrew.allison@digitalinfamy.com>
2021-08-22 06:49:23 +0100

    chore(ci): trying to make the ci build work

--- .releaserc.js
+++ .releaserc.js
@@ -8,6 +8,50 @@ module.exports = {
     { name: 'alpha', prerelease: true }
   ],
   plugins: [
-    '@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github'
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
+        "message": "release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/commit-analyzer",
+    {
+      "releaseRules": [
+        {
+          "type": "build",
+          "release": "patch"
+        },
+        {
+          "type": "ci",
+          "release": "patch"
+        },
+        {
+          "type": "chore",
+          "release": "patch"
+        },
+        {
+          "type": "docs",
+          "release": "patch"
+        },
+        {
+          "type": "refactor",
+          "release": "patch"
+        },
+        {
+          "type": "style",
+          "release": "patch"
+        },
+        {
+          "type": "test",
+          "release": "patch"
+        }
+      ]
+    }
+  ],
   ]
 };
